f08b9b5078e23df398027bc6e269618c9c1dab00
zyl <zyl@localhost.com>
2020-04-24 10:29:44 +0800

    f

--- lib/mycfg.py
+++ lib/mycfg.py
@@ -14,12 +14,14 @@ class mycfg (object):
         ftp_info = []
         ftps = [section for section in self.config if section.startswith('ftp')]
         for host in ftps:
-            ftp_info.append(dict(self.config.items(host, raw=True)))
+            ftp_i = dict(self.config.items(host, raw=True))
+            ftp_i.setdefault('port', 21)
+            ftp_info.append(ftp_i)
         return ftp_info
 
     def get_db_cfg(self):
         ''' return ora_db_url,monitor_table_name '''
         db_cfg = dict(self.config.items('db', raw=True))
         url = "{}/{}@{}:{}/{}".format(db_cfg['username'], db_cfg['password'], 
-			              db_cfg['host'], db_cfg['port'], db_cfg['service_name'])
+			              db_cfg['host'], db_cfg.get('port', 1521), db_cfg['service_name'])
         return url,db_cfg.get('table_name', _default_table_name)
--- lib/myora.py
+++ lib/myora.py
@@ -3,6 +3,8 @@ import cx_Oracle
 class myora (object):
     ''' ref https://www.oracle.com/technetwork/cn/tutorials/229069-zhs.htm '''
     def __init__(self, url, table_name):
+        ''' init a oracle connect instance and a cursor, config the monitor table,
+        which would create the table if it not exist, and then truncate it '''
         self.con = cx_Oracle.connect(url)
         self.cur = self.con.cursor()
         self.table_name = table_name
@@ -10,6 +12,7 @@ class myora (object):
         self.__truncate_table()
 
     def __init_table(self):
+        ''' create monitor table when it is not exist '''
         try:
             check = 'select 1 from %s where rownum = 1' % self.table_name
             self.cur.execute(check)
@@ -24,9 +27,11 @@ class myora (object):
             self.cur.execute(create)
         
     def __truncate_table(self):
+        ''' truncate the monitor table '''
         self.cur.execute('truncate table {}'.format(self.table_name))
 
     def insert_table(self, rows):
+        ''' using batch insert rows to monitor table, it will using binding vars '''
         insert = "insert into %s values(:host, :dir, :filename, :bytes, :mdtm, :crtm)" % self.table_name
         self.cur.bindarraysize = 5000
         self.cur.executemany(insert, rows)
